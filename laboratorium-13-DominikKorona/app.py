8f1aefd66fcdea72dec9b86a1ac5caa0b52da877
DominikKorona <81295903+DominikKorona@users.noreply.github.com>
2023-01-21 17:38:48 +0100

    finish

--- laboratorium-13-DominikKorona/app.py
+++ laboratorium-13-DominikKorona/app.py
@@ -1,4 +1,5 @@
 from textblob import TextBlob
+from typing import List, Tuple
 
 def hello(name):
     output = f'Hello {name}'
@@ -13,3 +14,19 @@ def extract_sentiment(text):
 
 def text_contain_word(word: str, text: str):
     return word in text
+
+
+def bubblesort(a: list) -> Tuple[List, int]:
+    n = len(a)
+    zmiana = True
+    counter = 0
+    a_copy = a.copy()
+    while n > 1 and zmiana:
+        zmiana = False
+        for i in range(1, n):
+            if a_copy[i - 1] > a_copy[i]:
+                a_copy[i - 1], a_copy[i] = a_copy[i], a_copy[i - 1]
+                zmiana = True
+            counter += 1
+        n -= 1
+    return a_copy, counter
--- laboratorium-13-DominikKorona/test/test_app.py
+++ laboratorium-13-DominikKorona/test/test_app.py
@@ -1,6 +1,7 @@
 from app import hello
 from app import extract_sentiment
 from app import text_contain_word
+from app import bubblesort
 import pytest
 
 
@@ -10,21 +11,40 @@ def test_hello():
 
     assert got == want
 
+
 testdata1 = ["I think today will be a great day"]
+
 
 @pytest.mark.parametrize('sample', testdata1)
 def test_extract_sentiment(sample):
-
     sentiment = extract_sentiment(sample)
 
     assert sentiment > 0
 
+
 testdata2 = [
     ('There is a duck in this text', 'duck', True),
     ('There is nothing here', 'duck', False)
-    ]
+]
+
 
 @pytest.mark.parametrize('sample, word, expected_output', testdata2)
 def test_text_contain_word(sample, word, expected_output):
-
     assert text_contain_word(word, sample) == expected_output
+
+
+# I
+# def test_bubblesort():
+#     unsored_lst = [4, 3, 6, 2, 1]
+#     got = bubblesort(unsored_lst)
+#     want = [4, 3, 6, 2, 1]
+#     assert got[0] == want
+
+# II
+test_data3 = [([4, 3, 6, 2, 1], [1, 2, 3, 4, 6]),
+              ([9, 8, 7, 5, 4], [4, 5, 7, 8, 9])]
+
+
+@pytest.mark.parametrize('lst, expected_output', test_data3)
+def test_bubblesort(lst, expected_output):
+    assert bubblesort(lst)[0] == expected_output
